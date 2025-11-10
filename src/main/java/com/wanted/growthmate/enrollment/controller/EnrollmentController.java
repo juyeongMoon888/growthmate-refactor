8b691432e7fab1adc0fae6cc529b7217ad628a5d
JinYong833 <jjystudy1@gmail.com>
2025-11-10 19:34:00 +0900

    수강의 컨트롤러 입니다. 1. 전부 Post로 동작하게 구현하였는데 논의가 필요합니다.

---
+++ src/main/java/com/wanted/growthmate/enrollment/controller/EnrollmentController.java
@@ -0,0 +1,62 @@
+package com.wanted.growthmate.enrollment.controller;
+
+import com.wanted.growthmate.enrollment.dto.*;
+import com.wanted.growthmate.enrollment.entity.Enrollment;
+import com.wanted.growthmate.enrollment.service.EnrollmentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    @Autowired
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    // 수강 목록 생성
+    @PostMapping
+    public Enrollment createEnrollment(@RequestBody @Valid EnrollmentRequest request) {
+        return enrollmentService.createEnrollment(request);
+    }
+
+    // 유저 id와 강좌 id로 단일 조회
+    @PostMapping("/detail")
+    public EnrollmentResponse findEnrollmentByUserIdAndCourseId(@RequestBody @Valid EnrollmentRequest request) {
+        return enrollmentService.findEnrollmentByUserIdAndCourseId(request);
+    }
+
+    // 유저 id로 수강중인 강좌 목록 생성
+    @PostMapping("/list")
+    public List<EnrollmentResponse> findEnrollmentsByUserId(@RequestBody @Valid EnrollmentSearchRequest request) {
+        List<Enrollment> enrollments = enrollmentService.findEnrollmentByUserId(request);
+        return enrollments.stream()
+                .map(EnrollmentResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    // 강좌의 상태를 변경 (status)
+    @PatchMapping("/status")
+    public EnrollmentResponse updateEnrollmentStatus(@RequestBody @Valid EnrollmentStatusRequest request) {
+        return enrollmentService.updateEnrollmentStatus(request);
+    }
+
+    // 강좌의 진도율을 변경 (Progress)
+    @PatchMapping("/progress")
+    public EnrollmentResponse updateEnrollmentProgress(@RequestBody @Valid EnrollmentUpdateRequest request) {
+        return enrollmentService.updateEnrollmentProgress(request);
+    }
+
+    // userId, courseId로 조회 후
+    @DeleteMapping("/delete")
+    public String deleteEnrollment(@RequestBody @Valid EnrollmentRequest request) {
+        enrollmentService.deleteEnrollment(request);
+        return "Success";
+    }
+}
