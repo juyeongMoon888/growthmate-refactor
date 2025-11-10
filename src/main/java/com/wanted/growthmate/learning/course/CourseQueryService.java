26937e699b14fdcc5146b860f5a78f2e6fb11389
Juyeong Moon <whoor2828@gmail.com>
2025-11-10 17:39:13 +0900

    feat: 강좌 리스트 조회, 수정, 삭제 서비스/테스트 추가

--- src/main/java/com/wanted/growthmate/learning/course/CourseQueryService.java
+++ src/main/java/com/wanted/growthmate/learning/course/CourseQueryService.java
@@ -3,11 +3,14 @@ package com.wanted.growthmate.learning.course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CourseQueryService {
 
+    //TODO 리팩터링하기 @Autowired가 있는데 왜 또 생성자가?
     @Autowired
     private CourseRepository courseRepository;
 
@@ -15,8 +18,10 @@ public class CourseQueryService {
         this.courseRepository = courseRepository;
     }
 
+    //TODO DTO로
     public CourseDetailResponse createCourse(int tutorId, int courseCategoryId, String courseTitle, String courseDescription, String courseImageUrl, int coursePointAmount) {
 
+        //TODO builder or 생성자로 넣기
         Course newCourse = new Course();
 
         newCourse.setUserId(tutorId);
@@ -27,11 +32,51 @@ public class CourseQueryService {
         newCourse.setCourseState(CourseState.DRAFT);
         newCourse.setImageUrl(courseImageUrl);
 
+        // TODO 엔터티를 response 타입으로 치환 해주는 로직 캡슐화에대해 고민해볼 ㄱ서
         courseRepository.save(newCourse);
         return new CourseDetailResponse(newCourse.getTitle(), newCourse.getDescription(), newCourse.getUserId(), newCourse.getPointAmount(), newCourse.getImageUrl());
     }
 
     public Optional<Course> getCourse(int courseId) {
         return courseRepository.findById(courseId);
+    }
+
+    //TODO:  바로 return, .map으로 변환하기
+    public List<CourseDetailResponse> getCourses() {
+        // TODO 소스 리팩터링
+        List<CourseDetailResponse> res = new ArrayList<>();
+        courseRepository.findAll().stream().forEach(course -> res.add(
+                // TODO 엔터티를 response 타입으로 치환 해주는 로직 캡슐화에대해 고민해볼 ㄱ서
+                new CourseDetailResponse(
+                        course.getTitle(),
+                        course.getDescription(),
+                        course.getUserId(),
+                        course.getPointAmount(),
+                        course.getImageUrl())
+        ));
+        return res;
+    }
+
+    public CourseDetailResponse update(int course_id, CourseEdit courseEdit) throws IllegalAccessException {
+        Course course = courseRepository.findById(course_id)
+                .orElseThrow(() -> new IllegalAccessException("Course not found with id: " + course_id));
+
+        course.setTitle(courseEdit.getTitle());
+        course.setDescription(courseEdit.getDescription());
+        course.setCategoryId(courseEdit.getCategoryId());
+        course.setUserId(courseEdit.getUserId());
+        course.setPointAmount(courseEdit.getPointAmount());
+        course.setImageUrl(courseEdit.getImageUrl());
+
+        courseRepository.save(course);
+
+        return new CourseDetailResponse(course.getTitle(), course.getDescription(), course.getUserId(), course.getPointAmount(), course.getImageUrl());
+    }
+
+    public void deleteCourse(int course_id) {
+        Optional<Course> findCourse = courseRepository.findById(course_id);
+        if (findCourse.isPresent()) {
+            courseRepository.deleteById(course_id);
+        }
     }
 }
--- src/test/java/com/wanted/growthmate/learning/course/CourseQueryServiceTest.java
+++ src/test/java/com/wanted/growthmate/learning/course/CourseQueryServiceTest.java
@@ -1,9 +1,11 @@
 package com.wanted.growthmate.learning.course;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -47,4 +49,70 @@ class CourseQueryServiceTest {
         assertThat(course.getDescription()).isEqualTo("강좌 설명");
     }
 
+    @Test
+    void 강좌_리스트_조회() throws Exception {
+        CourseCreateRequest dto1 = new CourseCreateRequest();
+        dto1.setUserId(1); // 강사ID
+        dto1.setCategoryId(1);
+        dto1.setTitle("강좌 제목");
+        dto1.setDescription("강좌 설명");
+        dto1.setImageUrl("이미지url");
+        dto1.setPointAmount(200); // 200P
+
+        courseQueryService.createCourse(dto1.getUserId(), dto1.getCategoryId(), dto1.getTitle(), dto1.getDescription(), dto1.getImageUrl(), dto1.getPointAmount());
+
+        CourseCreateRequest dto2 = new CourseCreateRequest();
+        dto2.setUserId(1); // 강사ID
+        dto2.setCategoryId(1);
+        dto2.setTitle("강좌 제목2");
+        dto2.setDescription("강좌 설명2");
+        dto2.setImageUrl("이미지url2");
+        dto2.setPointAmount(500); // 200P
+
+        courseQueryService.createCourse(dto2.getUserId(), dto2.getCategoryId(), dto2.getTitle(), dto2.getDescription(), dto2.getImageUrl(), dto2.getPointAmount());
+
+        List<CourseDetailResponse> savedCourses = courseQueryService.getCourses();
+        assertThat(savedCourses).isNotEmpty();
+        assertThat(savedCourses).hasSize(2); //요소 길이
+    }
+
+    @Test
+    void 강좌_수정() throws Exception {
+        CourseCreateRequest dto3 = new CourseCreateRequest();
+        dto3.setUserId(1); // 강사ID
+        dto3.setCategoryId(1);
+        dto3.setTitle("강좌 제목");
+        dto3.setDescription("강좌 설명");
+        dto3.setImageUrl("이미지url");
+        dto3.setPointAmount(200); // 200P
+
+        courseQueryService.createCourse(dto3.getUserId(), dto3.getCategoryId(), dto3.getTitle(), dto3.getDescription(), dto3.getImageUrl(), dto3.getPointAmount());
+
+        CourseEdit courseEdit = new CourseEdit();
+        courseEdit.setUserId(1);
+        courseEdit.setCategoryId(1);
+        courseEdit.setTitle("강좌 수정");
+        courseEdit.setDescription("강좌 설명");
+        courseEdit.setImageUrl("이미지url");
+        courseEdit.setPointAmount(200);
+
+        CourseDetailResponse updatedCourse = courseQueryService.update(1, courseEdit);
+        org.junit.jupiter.api.Assertions.assertEquals(updatedCourse.getTitle(), "강좌 수정");
+    }
+
+    @Test
+    void 강좌_삭제() throws Exception {
+        CourseCreateRequest dto5 = new CourseCreateRequest();
+        dto5.setUserId(1); // 강사ID
+        dto5.setCategoryId(1);
+        dto5.setTitle("강좌 제목");
+        dto5.setDescription("강좌 설명");
+        dto5.setImageUrl("이미지url");
+        dto5.setPointAmount(200); // 200P
+
+        courseQueryService.createCourse(dto5.getUserId(), dto5.getCategoryId(), dto5.getTitle(), dto5.getDescription(), dto5.getImageUrl(), dto5.getPointAmount());
+
+        courseQueryService.deleteCourse(1);
+        assertThat(courseQueryService.getCourse(1)).isEmpty();
+    }
 }
