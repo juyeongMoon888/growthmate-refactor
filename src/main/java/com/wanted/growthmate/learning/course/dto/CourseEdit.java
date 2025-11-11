04ab3bc957e668bfbaecf6fc04dfe73860205b39
Juyeong Moon <whoor2828@gmail.com>
2025-11-11 11:09:30 +0900

    refactor: userId, categoryId 수정 필드에서 제거

--- src/main/java/com/wanted/growthmate/learning/course/dto/CourseEdit.java
+++ src/main/java/com/wanted/growthmate/learning/course/dto/CourseEdit.java
@@ -4,22 +4,13 @@ import lombok.Builder;
 
 @Builder
 public class CourseEdit {
-    private int userId;
     private String title; //변경이 없다면 기존과 같은값
-    private int categoryId;
     private String description;
     private int pointAmount;
     private String imageUrl;
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
     }
 
     public void setDescription(String description) {
@@ -34,16 +25,8 @@ public class CourseEdit {
         this.imageUrl = imageUrl;
     }
 
-    public int getUserId() {
-        return userId;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
     }
 
     public String getDescription() {
