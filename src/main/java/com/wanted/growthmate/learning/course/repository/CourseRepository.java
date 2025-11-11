af9682cdd39c74d9283f21e19c4beedb96b084ec
Juyeong Moon <whoor2828@gmail.com>
2025-11-11 19:04:23 +0900

    refactor: Category를 category.domain.entity로 이동 후 import 경로 수정

--- src/main/java/com/wanted/growthmate/learning/course/repository/CourseRepository.java
+++ src/main/java/com/wanted/growthmate/learning/course/repository/CourseRepository.java
@@ -1,6 +1,6 @@
 package com.wanted.growthmate.learning.course.repository;
 
-import com.wanted.growthmate.learning.course.domain.Course;
+import com.wanted.growthmate.learning.course.domain.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
