01e524608e5ac1f8e9ef1751bcb76fd82ad60fc8
sobee1403 <sobee1403@daum.net>
2025-11-12 17:48:38 +0900

    refactor: BaseEntity 계층 구조 및 equals/hashCode 개선
    - BaseEntity, BaseTimeEntity, SoftDeleteBaseEntity 상속 구조 분리
    - Hibernate 프록시 안전 비교를 위한 equals()/hashCode() 수정
    - createdAt/updatedAt 필드명 통일
    - JPA 리플렉션 생성을 위한 기본 생성자(protected) 추가

--- src/main/java/com/wanted/growthmate/common/entity/BaseEntity.java
+++ src/main/java/com/wanted/growthmate/common/entity/BaseEntity.java
@@ -17,16 +17,25 @@ public abstract class BaseEntity {
         return id;
     }
 
+    protected BaseEntity() {
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null) return false;
+
+        // 프록시 안전 비교
+        if (org.hibernate.Hibernate.getClass(this) != org.hibernate.Hibernate.getClass(o)) return false;
         BaseEntity that = (BaseEntity) o;
-        return Objects.equals(id, that.id);
+
+        // 영속화 전(id == null)에는 '동등 아님'으로 처리
+        return this.id != null && this.id.equals(that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        // 변하지 않는 클래스 해시 사용 (프록시 안전성을 위해 Hibernate.getClass 사용해도 됨)
+        return org.hibernate.Hibernate.getClass(this).hashCode();
     }
 }
