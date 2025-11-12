0f2a75345e144f3afd2f4b489acec8052764aeec
dawwson <shines1427@gmail.com>
2025-11-12 23:30:28 +0900

    comment: 서비스 메서드에 주석 작성

--- src/main/java/com/wanted/growthmate/payment/service/PointTransactionServiceImpl.java
+++ src/main/java/com/wanted/growthmate/payment/service/PointTransactionServiceImpl.java
@@ -1,7 +1,6 @@
 package com.wanted.growthmate.payment.service;
 
 import com.wanted.growthmate.payment.domain.Point;
-import com.wanted.growthmate.payment.domain.PointTransaction;
 import com.wanted.growthmate.payment.dto.PointTransactionSummary;
 import com.wanted.growthmate.payment.repository.PointTransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +11,12 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PointTransactionServiceImpl implements PointTransactionService {
+
     private final PointTransactionRepository pointTransactionRepository;
 
+    /*
+     * 로그인한 사용자의 포인트 전체 내역 조회
+     */
     public List<PointTransactionSummary> getTransactionSummaries(Point point) {
         return pointTransactionRepository.findSummariesByPoint(point);
     }
