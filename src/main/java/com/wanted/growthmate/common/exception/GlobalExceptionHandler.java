3105f2a4dd0b6fe79844ebdf70c5eb3755b641a4
woongblack <woongblack123@gmail.com>
2025-11-13 17:46:16 +0900

    feat : ExceptionHandler(valid) 추가

--- src/main/java/com/wanted/growthmate/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wanted/growthmate/common/exception/GlobalExceptionHandler.java
@@ -2,9 +2,16 @@ package com.wanted.growthmate.common.exception;
 
 import com.wanted.growthmate.learning.lecture.exception.LectureNotFoundException;
 import com.wanted.growthmate.learning.section.exception.SectionNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,6 +33,22 @@ public class GlobalExceptionHandler {
         model.addAttribute("error", "오류가 발생했습니다: " + e.getMessage());
         e.printStackTrace();
         return "error/500";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+            // (FieldError)로 캐스팅하여 필드 이름과 메시지를 가져옵니다.
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
     }
 }
 
